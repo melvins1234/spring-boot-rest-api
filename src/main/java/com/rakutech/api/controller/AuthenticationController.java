5691a334008df3d8c0132cb75c90db9e8e9fc865
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2021-07-03 01:05:44 +0800

    JWT token partially added

---
+++ src/main/java/com/rakutech/api/controller/AuthenticationController.java
@@ -0,0 +1,51 @@
+package com.rakutech.api.controller;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rakutech.api.dto.Auth;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@RestController
+public class AuthenticationController {
+	
+	@PostMapping("auth")
+	public Auth log(@RequestParam("user") String username, 
+					@RequestParam("password") String pwd) {
+		String token = getJWTToken(username);
+		Auth auth = new Auth();
+		auth.setUser(username);
+		auth.setToken(token);
+		return auth;
+	}
+	
+	private String getJWTToken(String username) {
+		String secretKey = "mySecretKey";
+		List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+				.commaSeparatedStringToAuthorityList("ROLE_USER");
+		
+		String token = Jwts
+				.builder()
+				.setId("softtekJWT")
+				.setSubject(username)
+				.claim("authorities",
+						grantedAuthorities.stream()
+								.map(GrantedAuthority::getAuthority)
+								.collect(Collectors.toList()))
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 600000))
+				.signWith(SignatureAlgorithm.HS512,
+						secretKey.getBytes()).compact();
+
+		return "Bearer " + token;
+	}
+}
