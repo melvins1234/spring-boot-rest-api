2b741dd367ea9f4d69c858a570ccba30d1547883
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2021-07-04 19:48:50 +0800

    Added JWT

--- src/main/java/com/rakutech/api/controller/AuthenticationController.java
+++ src/main/java/com/rakutech/api/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rakutech.api.dto.Auth;
+import com.rakutech.api.model.Administrator;
+import com.rakutech.api.repository.AdministratorRepository;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -18,13 +21,24 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @RestController
 public class AuthenticationController {
 	
+	@Autowired
+	AdministratorRepository administratorRepository;
+	
 	@PostMapping("auth")
 	public Auth log(@RequestParam("user") String username, 
 					@RequestParam("password") String pwd) {
-		String token = getJWTToken(username);
+		
 		Auth auth = new Auth();
-		auth.setUser(username);
-		auth.setToken(token);
+		
+		administratorRepository.findAll().forEach(e -> {
+			if(e.getUsername().equals(username) && e.getPassword().equals(pwd)) {
+				String token = getJWTToken(username);
+				auth.setUser(username);
+				auth.setPwd(pwd);
+				auth.setToken(token);
+			}
+		});
+		
 		return auth;
 	}
 	
